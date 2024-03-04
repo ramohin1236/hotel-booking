89ade1059479de38774a70eff9f0a1bb2c039f45
ramohin1236 <108376758+ramohin1236@users.noreply.github.com>
2024-03-04 11:27:00 -0800

    top destinatiosn created

--- src/pages/Home/HotelName/HotelName.jsx
+++ src/pages/Home/HotelName/HotelName.jsx
@@ -1,15 +1,52 @@
 /* eslint-disable react/no-unescaped-entities */
+import { useEffect, useState } from 'react';
 import Container from './../../../components/Shared/Container';
-
+import HotelNameCart from './HotelNameCart';
+import { TypeAnimation } from 'react-type-animation';
 
 const HotelName = () => {
+    const [hotelname,setHotelname]=useState([]);
+
+    useEffect(()=>{
+        fetch("/public/hotelname.json")
+        .then(res=>res.json())
+        .then(data=>setHotelname(data))
+    },[])
     return (
         <Container>
         <div className='mt-16 mb-16'>
-          <p className='text-4xl font-bold h-12'>Top destinations for Cox's Bazar city trips</p>
-          <p className='text-xl '>Find hotels in some of the most popular city at Cox's Bazar</p>
+     
+        <p className='text-4xl font-bold mb-6 text-blue-700 '>Top destinations for Cox's Bazar city trips</p>
+          <p className='text-xl text-slate-600'>Find hotels in some of the most popular city at Cox's Bazar</p>
         </div>
-    </Container>
+    
+           <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 '>
+                  {
+                    hotelname.map((hltname)=><HotelNameCart key={hltname.id}
+                    hltname={hltname}/>)
+                  }
+           </div>
+                <div className='text-end'>
+                <TypeAnimation
+            className=' text-end max-sm:text-3xl max-sm:ml-10 text-xl font-bold '
+      sequence={[
+        1000,
+        'For more information and booking for rooms go to "Rooms"',
+        
+        
+        // Types 'One'
+       
+        () => {
+          console.log('Sequence completed');
+        },
+      ]}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+     
+    />
+                </div>
+     </Container>
     );
 };
 
--- src/pages/Home/HotelName/HotelNameCart.jsx
+++ src/pages/Home/HotelName/HotelNameCart.jsx
@@ -1,9 +1,24 @@
+/* eslint-disable react/prop-types */
+import "./HotelNameCart.css";
 
 
-const HotelNameCart = () => {
+
+const HotelNameCart = ({hltname}) => {
+    const {hotel_name
+,image,location    }=hltname
+    console.log(hltname);
     return (
-        <div>
-            
+        <div className="mb-16">
+            <div className="w-full h-full borderr">
+            <img className="w-full h-80 rounded-md" src={image} alt="" />
+           <div className="ml-2 mt-4 mb-6">
+           <p className="text-2xl font-bold text-blue-700">{hotel_name}</p>
+            <p className="text-slate-600">{location}</p>            
+           </div>
+            </div>
+
+          
+           
         </div>
     );
 };
